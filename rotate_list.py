b7b1b854fe68da1bab908c69d3447808fdac6c41
rishika <rishikaprs@gmail.com>
2021-10-30 22:48:37 +0530

    Given a sorted list that was rotated unknown number of times, find the number of times it was rotated,

---
+++ rotate_list.py
@@ -0,0 +1,5 @@
+# Given a sorted list that was rotated unknown number of times, find the number of times it was rotated,
+# nums: a sorted rotated list
+# count_rotations : returns the number of times the list is rotated.
+def count_rotations:
+
