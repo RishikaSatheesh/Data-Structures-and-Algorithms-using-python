c9cf11ef6f06a95c8f6612d8cc4ee54ace54590e
rishika <rishikaprs@gmail.com>
2021-11-02 23:21:26 +0530

    As a backend angineer, you are tasked with developing a fast in-memory data structure to manage profile information (username,name and email) for 100 million users. It should allow the following operation to e performed efficiently: #INSERT the profile info for a new user. #FIND the profile info of a user, given username. #UPDATE the profile info of a user, given username #LIST all the users of the platform, sorted by username.

---
+++ binary_tree.py
@@ -0,0 +1,14 @@
+#As a backend angineer, you are tasked
+#with developing a fast in-memory data
+#structure to manage profile information (username,
+#name and email) for 100 million users.
+#It should allow the following operation to e performed
+#efficiently:
+#INSERT the profile info for a new user.
+#FIND the profile info of a user, given username.
+#UPDATE the profile info of a user, given username
+#LIST all the users of the platfor, sorted by username.
+
+
+# SOLUTION:
+
